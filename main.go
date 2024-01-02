b358320929475efed0774332a56bdb63dc31e563
isaac cobb <ic@ic-debian-macbook>
2024-01-01 19:47:21 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+    "strings"
+    "strconv"
+)
+
+var data[]string
+
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./static/index.html")
+}
+
+func getData(w http.ResponseWriter, r *http.Request) {
+	var output string
+	for i, s := range data {
+		output += fmt.Sprintf("<button hx-post=\"/remove/%d\">X</button><h1>%s</h1>", i, s)
+	}
+
+	fmt.Fprintf(w, "%s", output)
+}
+
+func handleRemove(w http.ResponseWriter, r *http.Request) {
+    idStr := strings.TrimPrefix(r.URL.Path, "/remove/")
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    data = append(data[:id], data[id+1:]...)
+}
+
+
+func handleClick(w http.ResponseWriter, r *http.Request) {
+	value := r.FormValue("input")
+	data = append(data, string(value))
+}
+
+func main() {
+	http.HandleFunc("/static", serveFile)
+	http.HandleFunc("/add", handleClick)
+	http.HandleFunc("/data", getData)
+	http.HandleFunc("/remove/", handleRemove)
+	fmt.Println("Listening on port 6969")
+	err := http.ListenAndServe(":6969", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
